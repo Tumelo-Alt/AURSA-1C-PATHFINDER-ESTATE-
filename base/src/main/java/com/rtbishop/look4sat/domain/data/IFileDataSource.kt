63de525310e484e21a38562e266c8d396ba06b5a
Tumelo-Alt <150380189+Tumelo-Alt@users.noreply.github.com>
2024-12-22 07:25:59 +0200

    Update IFileDataSource.kt

--- base/src/main/java/com/rtbishop/look4sat/domain/data/IFileDataSource.kt
+++ base/src/main/java/com/rtbishop/look4sat/domain/data/IFileDataSource.kt
@@ -1,7 +1,7 @@
 /*
  * Look4Sat. Amateur radio satellite tracker and pass predictor.
  * Copyright (C) 2019-2022 Arty Bishop (bishop.arty@gmail.com)
- *
+ */
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -14,7 +14,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+ *
 package com.rtbishop.look4sat.domain.data
 
 import java.io.InputStream
